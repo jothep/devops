57a1b42f0c9bfcaaed581453ada5b1287e82e309
jothep <190911232@qq.com>
2018-03-01 11:56:11 +0800

    Update getpodstat.py

--- podstatus/getpodstat.py
+++ podstatus/getpodstat.py
@@ -5,6 +5,27 @@ import sys
 import re
 import json,urllib2
 
+#transfer pods_string to list
+def strtolist(list,str):
+    list.append(str)
+    print list
+    return list
+
+#write podlist to alarmfile
+def listtofile(list,file):
+    fo = open(file,"w")
+    for i in list:
+        ns = i.split('/')[0]
+        pod = i.split('/')[1]
+        lp = len(pod)
+        if lp>17:
+            podname = pod[:-17]
+        else:
+            podname = pod
+        full =str(ns+' '+podname+'\n')
+        fo.write(full)
+    fo.close()
+
 #delete duplicates row from tempfile
 def deldup(file):
     rFile = open(file, 'r')
@@ -26,8 +47,10 @@ def str_count(s):
     lp = len(pod)
     if  lp > 17:
         podname = pod[:-17]
+        print podname
     else:
         podname = pod
+        print podname
     full = 'echo '+ns+' '+podname+' >>/opt/podstlist'
     os.popen(full)
     
@@ -35,11 +58,14 @@ def str_count(s):
  
 cmd = 'kubectl get pods -a --all-namespaces -o json  | jq -r \'.items[] | select(.status.phase != "Running" or ([ .status.conditions[] | select(.type == "Ready" and .status == "False") ] | length ) == 1 ) | .metadata.namespace + "/" + .metadata.name\''
 ret = str(os.popen(cmd).read())
-deldup('/opt/podstlist')
+file1 = '/opt/podstlist'
+deldup(file1)
 if ret:
     output = "以下pod存在状态异常:\n" + ret
     re.split('\/n',output)
-    str_count(ret)
+    list1 = ret.split('\n')[:-1]
+    listtofile(list1,file1)
+#    str_count(ret)
 else:
     output = ""
 print output
@@ -57,10 +83,10 @@ header = {
     "charset": "utf-8"
     }
 if output:
+    print "存在问题"
     sendData = json.dumps(data)
     request = urllib2.Request(url,data = sendData,headers = header)
     urlopen = urllib2.urlopen(request)
     print urlopen.read()
 else:
     print "不存在pod异常。"
-
